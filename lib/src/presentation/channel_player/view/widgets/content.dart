359ad7066073a189de59807873f91175a9b5db75
julian <julian.ramos.calvo@gmail.com>
2023-08-22 11:41:45 +0200

    use context select instead of bloc builder

--- lib/src/presentation/channel_player/view/widgets/content.dart
+++ lib/src/presentation/channel_player/view/widgets/content.dart
@@ -7,8 +7,6 @@ import 'package:radio_app/src/presentation/channel_player/view/widgets/play_butt
 import 'package:radio_app/src/presentation/channel_player/view/widgets/radio_image.dart';
 import 'package:radio_app/src/presentation/channel_player/view/widgets/radio_info.dart';
 import 'package:go_router/go_router.dart';
-
-import '../../cubit/channel_player_state.dart';
 import 'appbar_widget.dart';
 
 class Content extends StatelessWidget {
@@ -18,40 +16,36 @@ class Content extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<ChannelPlayerCubit, ChannelPlayerState>(
-      builder: (context, state) {
-        return Scaffold(
-          backgroundColor: Colors.black,
-          appBar: AppBarWidget(
-            onClosePressed: () => context.pop(),
-          ),
-          body: SingleChildScrollView(
-            child: Column(
-              children: [
-                Padding(
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 30, vertical: 20),
-                  child: RadioImage(radioChannel: radioChannel),
-                ),
-                const SizedBox(height: 20),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 10),
-                  child: RadioInfo(radioChannel: radioChannel),
-                ),
-                const SizedBox(height: 50),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 20),
-                  child: PlayButton(
-                    isPlaying: state.isPlaying,
-                    onPlayPressed: () =>
-                        context.read<ChannelPlayerCubit>().playRadioChannel(),
-                  ),
-                ),
-              ],
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBarWidget(
+        onClosePressed: () => context.pop(),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 20),
+              child: RadioImage(radioChannel: radioChannel),
             ),
-          ),
-        );
-      },
+            const SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: RadioInfo(radioChannel: radioChannel),
+            ),
+            const SizedBox(height: 50),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: PlayButton(
+                isPlaying: context.select(
+                    (ChannelPlayerCubit cubit) => cubit.state.isPlaying),
+                onPlayPressed: () =>
+                    context.read<ChannelPlayerCubit>().playRadioChannel(),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
